5c858ad044ba79df9e9f9b043bc506e325626ff3
cykGit <yunke125@163.com>
2023-02-23 18:33:51 +0800

    更新

--- src/main/java/com/ck/check_bean/CheckItem.java
+++ src/main/java/com/ck/check_bean/CheckItem.java
@@ -4,13 +4,24 @@ import com.ck.check_bean.annotation.CheckValue;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 @Data
 @Accessors(chain = true)
 public class CheckItem {
+    private static Logger log = Logger.getLogger(CheckItem.class.getName());
 
-    public static CheckItem build(CheckValue checkValue){
+    /**
+     * 注解参数构建检查条件的对象
+     *
+     * @param checkValue
+     * @return
+     */
+    public static CheckItem build(CheckValue checkValue) {
         return new CheckItem()
                 .setValue("".equals(checkValue.value().trim()) ? null : checkValue.value())
                 .setRegexp(checkValue.regexp())
@@ -21,6 +32,25 @@ public class CheckItem {
                 .setMax(checkValue.max() == -999999999 ? null : checkValue.max())
                 .setMin(checkValue.min() == -999999999 ? null : checkValue.min())
                 .setName(checkValue.name());
+    }
+
+    public static CheckItem build(Map<String, Object> param) {
+        Objects.requireNonNull(param, "构建 CheckItem 时，条件对象不能为空");
+        CheckItem checkItem = new CheckItem();
+        param.forEach((k, v) -> {
+            if ("checkItemChild".equalsIgnoreCase(k)) return;
+            try {
+                Field field = CheckItem.class.getDeclaredField(k);
+                field.set(checkItem, v);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                log.info(String.format("构建 CheckItem 时未找到名称为[%s]的条件参数", k));
+            }
+        });
+        if (param.containsKey("checkItemChild") && param.get("checkItemChild") != null) {
+            checkItem.setCheckItemChild(new HashMap<>());
+            ((Map<String, Object>) param.get("checkItemChild")).forEach((ik, iv) -> checkItem.getCheckItemChild().put(ik, build((Map<String, Object>) iv)));
+        }
+        return checkItem;
     }
 
     /**
