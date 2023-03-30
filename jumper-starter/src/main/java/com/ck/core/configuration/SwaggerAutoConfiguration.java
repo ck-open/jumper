1291981face6433407e6a24ddc764e77fedfe812
cykGit <yunke125@163.com>
2023-03-30 16:18:28 +0800

    更新

--- jumper-starter/src/main/java/com/ck/core/configuration/SwaggerAutoConfiguration.java
+++ jumper-starter/src/main/java/com/ck/core/configuration/SwaggerAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.ck.core.configuration;
 
 
 import com.ck.core.properties.SwaggerProperties;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.github.xiaoymin.knife4j.spring.extension.ApiAuthorExtension;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -22,12 +23,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
 import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.Resource;
 import javax.validation.constraints.NotNull;
@@ -43,6 +46,9 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @ConditionalOnProperty(prefix = "knife4j.swagger", name = "enabled", havingValue = "true")
+@EnableKnife4j
+@EnableSwagger2
+@EnableOpenApi
 @Configuration
 @EnableConfigurationProperties({SwaggerProperties.class})
 public class SwaggerAutoConfiguration {
@@ -75,18 +81,6 @@ public class SwaggerAutoConfiguration {
         }
     }
 
-    /**
-     * Knife4j  Swagger  配置注入
-     *
-     * @return
-     */
-    @Bean
-    @ConditionalOnMissingBean(Knife4jConfigurer.class)
-    public Knife4jConfigurer knife4jConfigurer() {
-        Knife4jConfigurer webConfigurer = new Knife4jConfigurer();
-        log.info("Knife4jConfigurer [{}]", webConfigurer);
-        return webConfigurer;
-    }
 
 
     /**
