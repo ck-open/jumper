96f37090c7fc4c23bd1fdd1a53764f830a8d5311
cykGit <yunke125@163.com>
2023-03-31 17:50:34 +0800

    更新

--- jumper-tools/src/main/java/com/ck/encrypt/EncryptUtil.java
+++ jumper-tools/src/main/java/com/ck/encrypt/EncryptUtil.java
@@ -3,10 +3,16 @@ package com.ck.encrypt;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
@@ -19,46 +25,64 @@ import java.util.*;
  */
 public final class EncryptUtil {
     public static void main(String[] args) {
-        System.out.println(getUUID());
-        System.out.println(strToMD5("asdfg"));
-        System.out.println(strToMD5("asdfg","dsaf55"));
-        System.out.println(compileStr("dsagdfg",true));
-        System.out.println(compileStr("sadgfdgfdterf5465f6sd4gf5d-dgfd",true));
+//        System.out.println(getUUID());
+//        System.out.println(strToMD5("asdfg"));
+//        System.out.println(strToMD5("asdfg","dsaf55"));
+//        System.out.println(compileStr("dsagdfg",true));
+//        System.out.println(compileStr("sadgfdgfdterf5465f6sd4gf5d-dgfd",true));
+        test();
     }
 
-    private void test() throws Exception {
-        String sKey="1234567890123456";
-        String ivParameter="1234567890123456";
+    private static void test() {
+        String sKey = "1234567890123456";
+        String ivParameter = "1234567890123456";
 
         // 需要加密的字串
-        String cSrc = "123456";
-        System.out.println("加密前的字串是："+cSrc);
+        String cSrc = "";
+        System.out.println("加密前的字串是：" + cSrc);
         // 加密
-        String enString = EncryptUtil.encryptAES(cSrc,"utf-8",sKey,ivParameter);
-        System.out.println("加密后的字串是："+ enString);
+        String enString = EncryptUtil.encryptAES(cSrc, "utf-8", sKey, ivParameter);
+        System.out.println("加密后的字串是：" + enString);
         System.out.println("1jdzWuniG6UMtoa3T6uNLA==".equals(enString));
         // 解密
-        String DeString = EncryptUtil.decryptAES(enString,"utf-8",sKey,ivParameter);
+        enString="uNWj5agUeZyNi53zBVR7CndP3SWaUSIB0xlulmJ19m3kLBJH5bHZbcXfS6DO6PzK";
+        String DeString = EncryptUtil.decryptAES(enString, "utf-8", sKey, ivParameter);
         System.out.println("解密后的字串是：" + DeString);
     }
 
 
-
-
-
-
-    // 加密 AES-128-CBC
-    public static String encryptAES(String sSrc, String encodingFormat, String sKey, String ivParameter) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        byte[] raw = sKey.getBytes();
-        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-        byte[] encrypted = cipher.doFinal(sSrc.getBytes(encodingFormat));
-        return new BASE64Encoder().encode(encrypted);//此处使用BASE64做转码。
+    /**
+     * 加密 AES-128-CBC
+     * @param sSrc
+     * @param encodingFormat
+     * @param sKey
+     * @param ivParameter
+     * @return
+     */
+    public static String encryptAES(String sSrc, String encodingFormat, String sKey, String ivParameter) {
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            byte[] raw = sKey.getBytes();
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+            byte[] encrypted = cipher.doFinal(sSrc.getBytes(encodingFormat));
+            return new BASE64Encoder().encode(encrypted);//此处使用BASE64做转码。
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
+            throw new RuntimeException("AES-128-CBC 加密失败", e);
+        }
     }
-    // 解密 AES-128-CBC
-    public static String decryptAES(String sSrc, String encodingFormat, String sKey, String ivParameter){
+
+    /**
+     * 解密 AES-128-CBC
+     * @param sSrc
+     * @param encodingFormat
+     * @param sKey
+     * @param ivParameter
+     * @return
+     */
+    public static String decryptAES(String sSrc, String encodingFormat, String sKey, String ivParameter) {
         try {
             byte[] raw = sKey.getBytes("ASCII");
             SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
@@ -67,10 +91,9 @@ public final class EncryptUtil {
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
             byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);//先用base64解密
             byte[] original = cipher.doFinal(encrypted1);
-            String originalString = new String(original,encodingFormat);
-            return originalString;
-        } catch (Exception ex) {
-            return null;
+            return new String(original, encodingFormat);
+        } catch (Exception e) {
+            throw new RuntimeException("AES-128-CBC 解密失败", e);
         }
     }
 
