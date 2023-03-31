2972792d719d4ed21bfde8abc7fac15a996b4292
cykGit <yunke125@163.com>
2023-03-31 18:32:54 +0800

    更新

--- jumper-tools/src/main/java/com/ck/function/compiler/CompilerUtils.java
+++ jumper-tools/src/main/java/com/ck/function/compiler/CompilerUtils.java
@@ -4,11 +4,13 @@ import com.ck.utils.FileUtil;
 
 import javax.tools.*;
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -41,87 +43,103 @@ public final class CompilerUtils {
 
         FileUtil.writerTextFile(path+"Sum.java",sb.toString());
 
-        compiler(path,FileUtil.getFilePath(path+"\\Sum").getPath());
+//        compiler(path,FileUtil.getFilePath(path+"\\Sum").getPath());
 
     }
 
 
-    public static void compile(String sourceCodeFile, File outPath,String... ops) {
-        Objects.requireNonNull(sourceCodeFile, "原代码为空");
+//    public static void testInvoke(String className, String source)
+//            throws ClassNotFoundException, IllegalAccessException,
+//            InstantiationException, NoSuchMethodException,
+//            InvocationTargetException {
+//
+//        final String SUFFIX = ".java";// 类名后面要跟的后缀
+//
+//        // 对source进行编译生成class文件存放在Map中，这里用bytecode接收
+//        Map<String, byte[]> bytecode = DynamicLoader.compile(className + SUFFIX,
+//                source);
+//
+//        // 加载class文件到虚拟机中，然后通过反射执行
+//        @SuppressWarnings("resource")
+//        DynamicLoader.MemoryClassLoader classLoader = new DynamicLoader.MemoryClassLoader(
+//                bytecode);
+//        Class<?> clazz = classLoader.loadClass("TestClass");
+//        Object object = clazz.newInstance();
+//
+//        // 得到sayHello方法
+//        Method sayHelloMethod = clazz.getMethod("sayHello", String.class);
+//        sayHelloMethod.invoke(object, "This is the method called by reflect");
+//
+//        // 得到add方法
+//        Method addMethod = clazz.getMethod("add", int.class, int.class);
+//        Object returnValue = addMethod.invoke(object, 1024, 1024);
+//        System.out.println(Thread.currentThread().getName() + ": "
+//                + "1024 + 1024 = " + returnValue);
+//
+//        // 因为在main方法中，调用了add和sayHello方法，所以直接调用main方法就可以执行两个方法
+//        Method mainMethod = clazz.getDeclaredMethod("main", String[].class);
+//        mainMethod.invoke(null, (Object) new String[] {});
+//    }
 
-        JavaCompiler javaCompiler = getJavaCompiler();
-        InputStream first = null; // 程序的输入  null 用 system.in
-        OutputStream second = null; // 程序的输出 null 用 system.out
-        OutputStream third = null;  // 程序的错误输出 .,null 用 system.err
-        // 程序编译参数 注意 我们编译目录是我们的项目目录
-//        String[] strings = {"-d", ".", "test/Hello.java"};
-       //  "-Xlint:unchecked","-classpath",System.getProperty("java.class.path")
-//        // 0 表示成功， 其他表示出现了错误
-        int result = javaCompiler.run(first, second, third,ops);
-        log.info("Java原代码动态编译" + (result == 0 ? "成功" : "失败"));
+
+    /**
+     * 通过类名和其代码（Java代码字符串），编译得到字节码，返回类名及其对应类的字节码，封装于Map中，值得注意的是，
+     * 平常类中就编译出来的字节码只有一个类，但是考虑到内部类的情况， 会出现很多个类名及其字节码，所以用Map封装方便。
+     *
+     * @param javaName 类名
+     * @param javaSrc  Java源码
+     * @return map
+     */
+    public static Map<String, byte[]> compile(String javaName, String javaSrc) {
+        // 调用java编译器接口
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager stdManager = compiler
+                .getStandardFileManager(null, null, null);
+
+        try (MemoryJavaFileManager manager = new MemoryJavaFileManager(
+                stdManager)) {
+
+            @SuppressWarnings("static-access")
+            JavaFileObject javaFileObject = manager.makeStringSource(javaName,
+                    javaSrc);
+            JavaCompiler.CompilationTask task = compiler.getTask(null, manager,
+                    null, null, null, Arrays.asList(javaFileObject));
+            if (task.call()) {
+                return manager.getClassBytes();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
-     * 获取 JavaCompiler 编译器
+     * 先根据类名在内存中查找是否已存在该类，若不存在则调用 URLClassLoader的 defineClass方法加载该类
+     * URLClassLoader的具体作用就是将class文件加载到jvm虚拟机中去
      *
-     * @return
+     * @author Administrator
+     *
      */
-    public static JavaCompiler getJavaCompiler() {
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    public static class MemoryClassLoader extends URLClassLoader {
+        Map<String, byte[]> classBytes = new HashMap<String, byte[]>();
 
-//        //获取java文件管理类
-//        StandardJavaFileManager manager = compiler.getStandardFileManager(null, null, null);
-//
-//        //获取java文件对象迭代器
-//        Iterable<? extends JavaFileObject> it = manager.getJavaFileObjects("/javaFile.java");
-//
-//        //设置编译参数
-//        ArrayList ops = new ArrayList();
-//        ops.add("-Xlint:unchecked");
-//        //设置classpath  当我们要编译的源代码中，引用了其他代码，我们需要将引用代码路径设置到-classpath中，否则会编译失败。
-//        ops.add("-classpath");
-//        //获取编译任务
-//        JavaCompiler.CompilationTask task = compiler.getTask(null, manager, null, ops, null, it);
-//        // 执行编译任务
-//        task.call();
+        public MemoryClassLoader(Map<String, byte[]> classBytes) {
+            super(new URL[0], MemoryClassLoader.class.getClassLoader());
+            this.classBytes.putAll(classBytes);
+        }
 
-        Objects.requireNonNull(compiler, "获取 JavaCompiler 编译器失败");
-        return compiler;
-    }
-
-
-
-
-
-    public static void compiler(String outPath,String... sources) throws IOException {
-        //1.获得系统编译器
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-
-        //2. 建立DiagnosticCollector对象
-        DiagnosticCollector<Object> diagnosticCollector = new DiagnosticCollector<>();
-        StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnosticCollector, null, null);
-
-        //3. 建立源文件对象，每一个文件都被保存在一个JavaFileObject继承的类中
-        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromStrings(
-                Arrays.asList(sources));
-
-        //4. 确定options命令行选项
-        List<String> options = Arrays.asList("-d", outPath);
-
-        //5. 获取编译任务
-        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnosticCollector, options, null, compilationUnits);
-
-        //6. 编译源程序
-        Boolean success = task.call();
-        fileManager.close();
-        System.out.println(success ? "编译成功" : "编译失败");
-
-        //7. 打印信息
-        for (Diagnostic<?> diagnostic : diagnosticCollector.getDiagnostics()) {
-            System.out.printf("Code: %s%n" + "Kind: %s%n" + "Position: %s%n" + "Start Position: %s%n"
-                            + "End Position: %s%n" + "Source: %s%n" + "Message: %s%n", diagnostic.getCode(),
-                    diagnostic.getKind(), diagnostic.getPosition(), diagnostic.getStartPosition(),
-                    diagnostic.getEndPosition(), diagnostic.getSource(), diagnostic.getMessage(null));
+        @Override
+        protected Class<?> findClass(String name)
+                throws ClassNotFoundException {
+            byte[] buf = classBytes.get(name);
+            if (buf == null) {
+                return super.findClass(name);
+            }
+            classBytes.remove(name);
+            return defineClass(name, buf, 0, buf.length);
         }
     }
+
+
 }
