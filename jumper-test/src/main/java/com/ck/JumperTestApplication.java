31572ff253cb0b7bedd1722c99800f6fd9ca4049
cykGit <yunke125@163.com>
2023-04-09 09:00:55 +0800

    更新

--- jumper-test/src/main/java/com/ck/JumperTestApplication.java
+++ jumper-test/src/main/java/com/ck/JumperTestApplication.java
@@ -4,13 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @ClassName EurekaApplication
- * @Description Gateway启动类
- * @Author Cyk
- * @Version 1.0
- * @since 2022/5/5 17:03
- **/
+
 @MapperScan(basePackages = "com.ck.db.mapper")
 @SpringBootApplication
 public class JumperTestApplication {
